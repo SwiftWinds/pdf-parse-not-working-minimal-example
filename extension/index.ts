1e15ef31c245b641ad54a6079392ec835ca91fa3
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2024-10-29 16:15:12 -0700

    Create a minimal example of pdf-parse not working

--- extension/index.ts
+++ extension/index.ts
@@ -1,12 +1,37 @@
-import { commands, ExtensionContext } from 'vscode';
-import { MainPanel } from './views/panel';
+import { commands, ExtensionContext, window } from "vscode";
+import { MainPanel } from "./views/panel";
+import fs from "fs";
+import pdf from "pdf-parse/lib/pdf-parse";
 
 export function activate(context: ExtensionContext) {
   // Add command to the extension context
   context.subscriptions.push(
-    commands.registerCommand('hello-world.showHelloWorld', async () => {
+    commands.registerCommand("hello-world.showHelloWorld", async () => {
       MainPanel.render(context);
-    }),
+    })
+  );
+
+  // Add command to parse a pdf file
+  context.subscriptions.push(
+    commands.registerCommand("hello-world.parsePdf", async () => {
+      // show a file picker
+      const file = await window.showOpenDialog({
+        canSelectFiles: true,
+        canSelectFolders: false,
+        canSelectMany: false,
+      });
+
+      if (!file) {
+        return;
+      }
+
+      // Read and parse PDF using pdf-parse
+      const dataBuffer = fs.readFileSync(file[0].fsPath);
+      const data = await pdf(dataBuffer);
+
+      // Print the text content to the console
+      console.log(data.text);
+    })
   );
 }
 
